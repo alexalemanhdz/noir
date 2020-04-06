89847f3caf123e15dae4f97c5ea0706914a2f732
alexalemanhdz <alex.aleman.public@gmail.com>
2020-04-05 21:22:01 -0500

    static files

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const port = 1337;
 
+app.locals.basedir = __dirname;
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('index'));
 
