c90560649a8941a14e2d921735613ad6b27a1fb4
alexalemanhdz <alex.aleman.public@gmail.com>
2020-04-05 21:14:03 -0500

    pugged

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 1337;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
